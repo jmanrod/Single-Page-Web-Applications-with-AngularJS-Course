d500f7273d9ea0df339308b5178dbcf26cbd51dc
jmanrod <egroj.mr@gmail.com>
2017-07-27 21:21:54 +0200

    Exercise finished

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -2,22 +2,38 @@
 'use strict';
 
 angular.module('LunchCheck', [])
-.controller('LunchCheckController', LunchCheckController)
+.controller('LunchCheckController', LunchCheckController);
 
 function LunchCheckController(){
   var lunch = this;
   lunch.itemsForLunch="";
 
   lunch.checkItemsForLunch = function(){
-    lunch.itemsForLunchSplitted = lunch.itemsForLunch.split(",");
+    lunch.style='solid';
+    lunch.itemsForLunchSplitted = clean(lunch.itemsForLunch.split(","));
     if(lunch.itemsForLunchSplitted.length <= 0 || lunch.itemsForLunch===""){
       lunch.message = "Please enter data first";
+      lunch.color="red";
     } else if (lunch.itemsForLunchSplitted.length <= 3){
       lunch.message = "Enjoy!";
+      lunch.color="green";
     } else {
       lunch.message = "Too much!";
+      lunch.color="green";
     }
-    console.log("itemsForLunchSplitted", lunch.itemsForLunchSplitted);
+  }
+
+  /**
+  * Delete empty elements
+  */
+  function clean(array){
+    var arrayTemp = new Array();
+    for (var i = 0; i < array.length; i++) {
+      if(array[i]){
+        arrayTemp.push(array[i]);
+      }
+    }
+    return arrayTemp;
   }
 }
 
